9467d9627de51587198792f88ec6914e27bc4473
Peter_Jiang <865268033@qq.com>
2019-01-16 11:21:46 +0800

    数据处理

--- Data_Processing.py
+++ Data_Processing.py
@@ -7,34 +7,44 @@
 """
 from __future__ import print_function
 import pandas as pd
-import numpy as np
 import re
 import warnings
+import math
 warnings.filterwarnings('ignore')
+
+
+def fillna_with_mean(data,column,value):
+    """利用均值填充"""
+    mean_val=cal_col_mean(data,column)
+    for i in range(len(data[column])):
+        if data[column].values[i]==value:
+            data[column].values[i]=mean_val
+    return data
 
 def time_2_sec(t):
     try:
         hours,minutes,sec=t.split(":")
     except:
         if t=="1900/1/9 7:00":
-            return (7*3600)/3600
+            return math.floor((7*3600)/3600)
         elif t=="1900/1/1 2:30":
-            return (2*3600+30*60)/3600
+            return math.floor((2*3600+30*60)/3600)
         elif t=="700":
-            return (7*3600)/3600
+            return math.floor((7*3600)/3600)
         elif t==-1:
             return -1
         else:
             return 0
     try:
-        sec=(int(hours)*3600+int(minutes)*60+int(sec))/3600
+        sec=math.floor((int(hours)*3600+int(minutes)*60+int(sec))/3600)
     except:
-        return (30*60)/3600
-
+        return math.floor((30*60)/3600)
+        # return 0
     return sec
 
 def get_last_time(t):
 
+    #需要考虑的情况就是后面的数字比前面的数字要小的情况
     try:
         start_hour,start_min,end_hour,end_min=re.split("[:,-]",t)
     except:
@@ -44,25 +54,6 @@ def get_last_time(t):
             return 3600/3600
         elif t=="13::30-15:00":
             return 5400/3600
-        # elif t=="2:00-4::00":
-        #     return 2
-        # elif t=="18::30-20:00":
-        #     return 5400/3600
-
-
-        # elif t=="18:00:-18:30":
-        #     return 30*60/3600
-        # elif t=="15:30-17;30":
-        #     return 2*3600/3600
-        # elif t=="15；00-17:00":
-        #     return 2*3600/3600
-        # elif t=="20:00-22;00":
-        #     return 2
-        # elif t=="20；00-22:00":
-        #     return 2
-        # elif t=="12:00-14;00":
-        #     return 2
-
         elif t=="16:30-17;30":
             return 3600/3600
         elif t=="6:00-8；00":
@@ -83,18 +74,22 @@ def get_last_time(t):
             return 3600/3600
         elif t=="11;30-13；00":
             return 5400/3600
-
         elif t==-1:
             return -1
         else:
             return 30*60/3600
+
     try:
+        # if int(end_hour)<int(start_hour):
+        #     sec=(int(end_hour)*3600+int(end_min)*60-int(start_hour)*3600-int(start_min)*60)/3600+24
+        # else:
         sec=(int(end_hour)*3600+int(end_min)*60-int(start_hour)*3600-int(start_min)*60)/3600
     except:
         if t=='19:-20:05':
             return (60*60+300)/3600
         else:
             return (30*60)/3600
+            # return 0
     return sec
 
 #只处理没有时段特征的数据
@@ -131,15 +126,8 @@ def adjust_time(column,data):
      """如果发现时间数据的时段前面大于后面，要么就去掉数据，要么就进行调整"""
      temp_dict={}
      for i in range(len(data[column])):
-         start_time,end_time=data[column].values[i].split('-')
-         start_hour_time,start_min_time=start_time.split(":") if len(start_time.split(":"))==2 else 0,0
-         end_hour_time,end_min_time=end_time.split(":") if len(end_time.split(":"))==2 else 0,0
+
          """需要考虑凌晨的数字比较小，所以需要进行特殊处理"""
-         if int(start_hour_time)*3600+int(start_min_time)*60>int(end_hour_time)*3600+int(end_min_time)*60:
-             # data[column].value[i]=end_time
-             # data[column].value[i]+='-'
-             # data[column].value[i]+=start_time
-             temp_dict[i]=data[column].values[i]
 
      return  column,temp_dict
 
@@ -161,39 +149,165 @@ def remove_columns_with_rate(train,ratio):
             temp_cols.remove(col)
     return temp_cols
 
+def Min_MAX(data,col):
+    max = data[col].max()
+    min = data[col].min()
+    for i in range(len(data[col])):
+        data[col].values[i] = (data[col].values[i] - min) / (max - min)
+    return data
+
+
 def Normalization(data,columns,fun_name):
     """利用相关函数对数据进行规范化操作"""
     for col in columns:
         try:
             data[col]=data[col].apply(fun_name)
-            max = data[col].max()
-            min = data[col].min()
-            for i in range(len(data[col])):
-                data[col].values[i] = (data[col].values[i] - min) / (max - min)
+            print(data[col])
+            Min_MAX(data,col)
+            # max = data[col].max()
+            # min = data[col].min()
+            # for i in range(len(data[col])):
+            #     data[col].values[i] = (data[col].values[i] - min) / (max - min)
         except:
             raise NotImplementedError("columns is None")
     return data
 
-def modify_anomaly_data(t):
-    pass
+def fenxiang(train,test,target,cate_columns):
+
+    train['target'] = target
+    train['intTarget'] = pd.cut(train['target'], 5, labels=False)
+    train = pd.get_dummies(train, columns=['intTarget'])
+    li = ['intTarget_0.0', 'intTarget_1.0', 'intTarget_2.0', 'intTarget_3.0', 'intTarget_4.0']
+    mean_columns = []
+    for f1 in cate_columns:
+        cate_rate = train[f1].value_counts(normalize=True, dropna=False).values[0]
+        if cate_rate < 0.90:
+            for f2 in li:
+                col_name = 'B14_to_' + f1 + "_" + f2 + '_mean'
+                mean_columns.append(col_name)
+                order_label = train.groupby([f1])[f2].mean()
+                train[col_name] = train['B14'].map(order_label)
+                miss_rate = train[col_name].isnull().sum() * 100 / train[col_name].shape[0]
+                if miss_rate > 0.0:
+                    train = train.drop([col_name], axis=1)
+                    mean_columns.remove(col_name)
+                else:
+                    test[col_name] = test['B14'].map(order_label)
+    for f1 in cate_columns:
+        cate_rate = train[f1].value_counts(normalize=True, dropna=False).values[0]
+        if cate_rate < 0.90:
+            for f2 in li:
+                col_name = 'B12_to_' + f1 + "_" + f2 + '_mean'
+                mean_columns.append(col_name)
+                order_label = train.groupby([f1])[f2].mean()
+                train[col_name] = train['B12'].map(order_label)
+                miss_rate = train[col_name].isnull().sum() * 100 / train[col_name].shape[0]
+                if miss_rate > 0.0:
+                    train = train.drop([col_name], axis=1)
+                    mean_columns.remove(col_name)
+                else:
+                    test[col_name] = test['B12'].map(order_label)
+    # train.drop(li + ['target'], axis=1, inplace=True)
+    return train,test,mean_columns
+
+
+
+"""计算缺失率=="""
+def cal_null_ratio(data,column):
+    return column,data[column].isnull().sum()/len(data[column])
+
+"""计算每个列特征的平均值"""
+def cal_col_mean(data,column):
+
+    if data[column].dtype=="float64":
+        sum=0.0
+    elif data[column].dtype=="int64":
+        sum=0
+    count=0
+
+    for i in range(len(data[column])):
+        if data[column].values[i]!=-1.0:
+            sum += data[column].values[i]
+            count+=1
+
+    return sum/count
+
+
+"""计算时间之间的间隔"""
+def get_gap_between_time(t1,t2):
+
+    """
+    example:
+        00:00:00
+        00:00:00-02:00:00
+    """
+    global sec
+    t1=str(t1)
+    t2=str(t2)
+    if t1.find('-')!=-1 and t2.find('-')!=-1:
+        try:
+            _,_,end_hour_1,end_min_1=re.split("[:,-]",t1)
+            start_hour_2,start_min_2,_,_=re.split("[:,-]",t2)
+            if start_hour_2<end_hour_1:
+                sec=(int(start_hour_2)*3600+int(start_min_2)*60-int(end_hour_1)*3600-int(end_min_1)*60+24*3600)/3600
+            else:
+                sec=(int(start_hour_2)*3600+int(start_min_2)*60-int(end_hour_1)*3600-int(end_min_1)*60)/3600
+            return sec
+        except:
+            return -1
+    elif t1.find('-')!=-1 and t2.find('-')==-1:
+        try:
+            _,_,end_hour_1,end_min_1=re.split("[:,-]",t1)
+            start_hour_2,start_min_2,_=re.split("[:]",t2)
+            if start_hour_2<end_hour_1:
+                sec = (int(start_hour_2) * 3600 + int(start_min_2) * 60 - int(end_hour_1) * 3600 - int(end_min_1) * 60+24*3600) / 3600
+            else:
+                sec = (int(start_hour_2) * 3600 + int(start_min_2) * 60 - int(end_hour_1) * 3600 - int(end_min_1) * 60) / 3600
+            return sec
+        except:
+            return -1
+    elif t1.find('-')==-1 and t2.find('-')!=-1:
+        try:
+            end_hour_1,end_min_1,_=re.split("[:]",t1)
+            start_hour_2,start_min_2,_,_=re.split("[:,-]",t2)
+            if start_hour_2<end_hour_1:
+                sec = (int(start_hour_2) * 3600 + int(start_min_2) * 60 - int(end_hour_1) * 3600 - int(end_min_1) * 60 + 24 * 3600) / 3600
+            else:
+                sec = (int(start_hour_2) * 3600 + int(start_min_2) * 60 - int(end_hour_1) * 3600 - int(end_min_1) * 60) / 3600
+            return sec
+        except:
+            return -1
+    elif t1.find('-')==-1 and t2.find('-')==-1:
+        try:
+            start_hour_1, start_min_1, _ = re.split("[:]", t1)
+            end_hour_1, end_min_1, _ = re.split("[:]", t2)
+            if end_hour_1<start_hour_1:
+                sec=(int(end_hour_1)*3600+int(end_min_1)*60-int(start_hour_1)*3600-int(start_min_1)*60+24*3600)/3600
+            else:
+                sec=(int(end_hour_1)*3600+int(end_min_1)*60-int(start_hour_1)*3600-int(start_min_1)*60)/3600
+            return sec
+        except:
+            return -1
+    else:
+        return -1
 
 def punction_process(t):
     pass
 
-
 if __name__=="__main__":
-    data=pd.read_csv("Data/jinnan_round1_train_20181227.csv",encoding = 'gb18030')
-    data['A24']=data['A24'].astype("str")
-    print(data.dtypes)
-    for i in ['A5','A9','A11','A14','A16','B7']:
-        print(find_anomaly_data(i,data))
-    data['B10']=data['B10'].astype(str)
-    data['B11']=data['B11'].astype(str)
-    for i in ['A20','A28','B4','B9']:
-        print(find_anomaly_data_2(i,data))
+    train = pd.read_csv('Data/jinnan_round1_train_20181227.csv', encoding='gb18030')
+    test = pd.read_csv('Data/jinnan_round1_testA_20181227.csv', encoding='gb18030')
 
-    for i in ['A20','A28','B4','B9',"B11","B10"]:
-        print(find_ano_data(i,data))
+    temp=[]
+    for i,j in zip(train["A9"].values,train["A11"].values):
+
+        sec=get_gap_between_time(i,j)
+        temp.append(sec)
+
+    print(len(temp))
+
+
+
 
 
 
